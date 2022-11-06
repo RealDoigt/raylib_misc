1bdc19a5f16b31b1185c20481f167f997340ed41
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-11-06 15:40:51 -0500

    clickable image

---
+++ lib/images/clickable.d
@@ -0,0 +1,58 @@
+module images.clickable;
+import images;
+
+class ClickableImage : Rect
+{
+    private
+    {
+        Img normalImage, hoverImage, activeImage;
+        Tx2d texture;
+    }
+
+    this(int x, int y, string imagePath, string hoverImagePath, string activeImagePath)
+    {
+        super(x, y, 0, 0, transparent);
+
+        normalImage = new Img(imagePath);
+        hoverImage  = new Img(hoverImagePath);
+        activeImage = new Img(activeImagePath);
+
+        texture = new Tx2d(normalImage);
+
+        width  = normalImage.width;
+        height = normalImage.height;
+
+        if (hoverImage.width != width || hoverImage.height != height)
+            hoverImage.resize(width, height);
+
+        if (activeImage.width != width || activeImage.height != height)
+            activeImage.resize(width, height);
+    }
+
+    // call this after calling onHover(), not before
+    void onClick(/*void delegate() d*/)
+    {
+        if (MOUSE_LEFT_BUTTON.IsMouseButtonPressed && checkCollision(GetMousePosition))
+        {
+            texture.update(activeImage);
+            //d();
+        }
+    }
+
+    void onHover()
+    {
+        if (checkCollision(GetMousePosition))
+            texture.update(hoverImage);
+    }
+
+    void onExit()
+    {
+        if (!checkCollision(GetMousePosition))
+            texture.update(normalImage);
+    }
+
+    override void draw()
+    {
+        texture.draw(x, y);
+    }
+}
--- lib/images/package.d
+++ lib/images/package.d
@@ -1,3 +1,7 @@
 module raylib_misc.images;
-public import raylib_misc.images.texture;
-public import raylib_misc.images.image;
+public
+{
+    import raylib_misc.images.texture;
+    import raylib_misc.images.image;
+    import raylib_misc.images.clickable;
+}
