b01211abe059b4eea0b6a75412e468ed7ce48ddc
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-11-06 16:19:30 -0500

    simplifying translucent colour enums
    applying Schveiguy's suggestion

--- lib/colors/translucent.d
+++ lib/colors/translucent.d
@@ -1,57 +1,14 @@
 module raylib_misc.colors.translucent;
 import raylib;
 
-enum Color white32    = {255, 255, 255,  32};
-enum Color white64    = {255, 255, 255,  64};
-enum Color white128   = {255, 255, 255, 128};
-enum Color white192   = {255, 255, 255, 192};
-
-enum Color black32    = {  0,   0,   0,  32};
-enum Color black64    = {  0,   0,   0,  64};
-enum Color black128   = {  0,   0,   0, 128};
-enum Color black192   = {  0,   0,   0, 192};
-
-enum Color red32      = {255,   0,   0,  32};
-enum Color red64      = {255, 255, 255,  64};
-enum Color red128     = {255, 255, 255, 128};
-enum Color red192     = {255, 255, 255, 192};
-
-enum Color orange32   = {255, 128,   0,  32};
-enum Color orange64   = {255, 128,   0,  64};
-enum Color orange128  = {255, 128,   0, 128};
-enum Color orange192  = {255, 128,   0, 192};
-
-enum Color yellow32   = {255, 255,   0,  32};
-enum Color yellow64   = {255, 255,   0,  64};
-enum Color yellow128  = {255, 255,   0, 128};
-enum Color yellow192  = {255, 255,   0, 192};
-
-enum Color green32    = {  0, 255,   0,  32};
-enum Color green64    = {  0, 255,   0,  64};
-enum Color green128   = {  0, 255,   0, 128};
-enum Color green192   = {  0, 255,   0, 192};
-
-enum Color blue32     = {  0,   0, 255,  32};
-enum Color blue64     = {  0,   0, 255,  64};
-enum Color blue128    = {  0,   0, 255, 128};
-enum Color blue192    = {  0,   0, 255, 192};
-
-enum Color cyan32     = {  0, 255, 255,  32};
-enum Color cyan64     = {  0, 255, 255,  64};
-enum Color cyan128    = {  0, 255, 255, 128};
-enum Color cyan192    = {  0, 255, 255, 192};
-
-enum Color magenta32  = {255,   0, 255,  32};
-enum Color magenta64  = {255,   0, 255,  64};
-enum Color magenta128 = {255,   0, 255, 128};
-enum Color magenta192 = {255,   0, 255, 192};
-
-enum Color brown32    = {128,  64, 0,    32};
-enum Color brown64    = {128,  64, 0,    64};
-enum Color brown128   = {128,  64, 0,   128};
-enum Color brown192   = {128,  64, 0,   192};
-
-enum Color purple32   = {128,   0, 255,  32};
-enum Color purple64   = {128,   0, 255,  64};
-enum Color purple128  = {128,   0, 255, 128};
-enum Color purple192  = {128,   0, 255, 192};
+enum Color whiteAplha(ubyte alpha)   = {255, 255, 255, alpha};
+enum Color blackAplha(ubyte alpha)   = {  0,   0,   0, alpha};
+enum Color redAplha(ubyte alpha)     = {255,   0,   0, alpha};
+enum Color orangeAplha(ubyte alpha)  = {255, 128,   0, alpha};
+enum Color yellowAplha(ubyte alpha)  = {255, 255,   0, alpha};
+enum Color greenAplha(ubyte alpha)   = {  0, 255,   0, alpha};
+enum Color blueAplha(ubyte alpha)    = {  0,   0, 255, alpha};
+enum Color cyanAplha(ubyte alpha)    = {  0, 255, 255, alpha};
+enum Color magentaAplha(ubyte alpha) = {255,   0, 255, alpha};
+enum Color brownAplha(ubyte alpha)   = {128,  64,   0, alpha};
+enum Color purpleAplha(ubyte alpha)  = {128,   0, 255, alpha};
