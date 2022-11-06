ff4026a9116029bbb5431a72bab7230b190b9ba9
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-11-06 16:21:03 -0500

    typo fixed
    Aplha -> Alpha

--- lib/colors/translucent.d
+++ lib/colors/translucent.d
@@ -1,14 +1,14 @@
 module raylib_misc.colors.translucent;
 import raylib;
 
-enum Color whiteAplha(ubyte alpha)   = {255, 255, 255, alpha};
-enum Color blackAplha(ubyte alpha)   = {  0,   0,   0, alpha};
-enum Color redAplha(ubyte alpha)     = {255,   0,   0, alpha};
-enum Color orangeAplha(ubyte alpha)  = {255, 128,   0, alpha};
-enum Color yellowAplha(ubyte alpha)  = {255, 255,   0, alpha};
-enum Color greenAplha(ubyte alpha)   = {  0, 255,   0, alpha};
-enum Color blueAplha(ubyte alpha)    = {  0,   0, 255, alpha};
-enum Color cyanAplha(ubyte alpha)    = {  0, 255, 255, alpha};
-enum Color magentaAplha(ubyte alpha) = {255,   0, 255, alpha};
-enum Color brownAplha(ubyte alpha)   = {128,  64,   0, alpha};
-enum Color purpleAplha(ubyte alpha)  = {128,   0, 255, alpha};
+enum Color whiteAlpha(ubyte alpha)   = {255, 255, 255, alpha};
+enum Color blackAlpha(ubyte alpha)   = {  0,   0,   0, alpha};
+enum Color redAlpha(ubyte alpha)     = {255,   0,   0, alpha};
+enum Color orangeAlpha(ubyte alpha)  = {255, 128,   0, alpha};
+enum Color yellowAlpha(ubyte alpha)  = {255, 255,   0, alpha};
+enum Color greenAlpha(ubyte alpha)   = {  0, 255,   0, alpha};
+enum Color blueAlpha(ubyte alpha)    = {  0,   0, 255, alpha};
+enum Color cyanAlpha(ubyte alpha)    = {  0, 255, 255, alpha};
+enum Color magentaAlpha(ubyte alpha) = {255,   0, 255, alpha};
+enum Color brownAlpha(ubyte alpha)   = {128,  64,   0, alpha};
+enum Color purpleAlpha(ubyte alpha)  = {128,   0, 255, alpha};
