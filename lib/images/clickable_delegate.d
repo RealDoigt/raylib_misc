3bd75aa32abab64b198ca2ed253399c69bdb2ebd
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-07 19:53:01 -0500

    Update clickable_delegate.d

--- lib/images/clickable_delegate.d
+++ lib/images/clickable_delegate.d
@@ -1,5 +1,6 @@
 module raylib_misc.images.clickable_delegate;
 import raylib_misc.images;
+import raylib;
 
 class ClickableImageDelegate : ClickableImageDelegate
 {
@@ -9,6 +10,11 @@ class ClickableImageDelegate : ClickableImageDelegate
     {
         super(x, y, imagePath, hoverImagePath, activeImagePath);
         this.clickAction = clickAction;
+    }
+
+    this(Vector xy, string imagePath, string hoverImagePath, string activeImagePath, void delegate() clickAction)
+    {
+        this(cast(int)position.x, cast(int)position.y, imagePath, hoverImagePath, activeImagePath, clickAction);
     }
 
     override void onClick()
