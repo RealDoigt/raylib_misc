f500d9b7b42ed54936331ad31d03757cf640e978
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-07 23:12:45 -0500

    Create music.d

---
+++ lib/audio/music.d
@@ -0,0 +1,20 @@
+module raylib_misc.audio.music;
+import raylib_misc;
+import std.string;
+import raylib;
+
+class MusicStream : IAudio
+{
+    private Music music;
+    alias music this;
+
+    this(string fileName)
+    {
+        music = fileName.toStringz.LoadMusicStream;
+    }
+
+    this(string fileType, const ubyte* data, int dataSize)
+    {
+        music = fileType.toStringz.LoadMusicStreamFromMemory(data, dataSize);
+    }
+}
