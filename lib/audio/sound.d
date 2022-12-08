8bbfead443ebe8b09262e34d0bb0d70174c320b6
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-07 23:54:14 -0500

    Update sound.d

--- lib/audio/sound.d
+++ lib/audio/sound.d
@@ -2,3 +2,29 @@ module raylib_misc.audio.sound;
 import raylib_misc;
 import std.string;
 import raylib;
+
+class Snd : IAudio
+{
+    private Sound sound;
+    alias sound this;
+
+    this(string fileName)
+    {
+        sound = fileName.toStringz.LoadWave;
+    }
+
+    this(Wave w)
+    {
+        sound = w.LoadSoundFromWave;
+    }
+
+    this(WaveSnd ws)
+    {
+        sound = LoadSoundFromWave(ws);
+    }
+
+    ~this()
+    {
+        sound.UnloadSound;
+    }
+}
