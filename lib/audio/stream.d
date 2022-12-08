c81e78cc783ae6f46ab430fc99d7093adbfea9d9
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-07 20:54:53 -0500

    Update stream.d

--- lib/audio/stream.d
+++ lib/audio/stream.d
@@ -1,3 +1,13 @@
 module raylib_misc.audio.stream;
 import raylib;
 
+class Audio : IAudio
+{
+    private AudioStream stream;
+    alias stream this;
+
+    this(uint sampleRate, uint sampleSize, uint channels)
+    {
+        stream = LoadAudioStream(sampleRate, sampleSize, channels);
+    }
+}
