e2c152cf813d80533320947c73879d32f046346d
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-29 15:21:56 -0500

    ctor + package

---
+++ lib/ui/package.d
@@ -0,0 +1,2 @@
+module raylib_misc.ui;
+public import raylib_misc.ui.rectcut;
---
+++ lib/ui/rectcut.d
@@ -0,0 +1,13 @@
+module raylib_misc.ui.rectcut;
+import raylib_misc.shapes.rectangle;
+import raylib_misc.colors.web;
+
+class RectCut : Rect
+{
+
+    this(int x, int y, int width, int height, Color color = black)
+    {
+        super(x, y, width, height, color);
+    }
+    
+}
