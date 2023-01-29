47b8f3c0fd827b813a2ba74addaa675fc435e5e4
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-29 15:23:53 -0500

    max properties

--- lib/ui/rectcut.d
+++ lib/ui/rectcut.d
@@ -4,10 +4,23 @@ import raylib_misc.colors.web;
 
 class RectCut : Rect
 {
+    private
+    {
+        auto maxX()
+        {
+            return width + x;
+        }
+        
+        auto maxY()
+        {
+            return height + y;
+        }
+    }
 
     this(int x, int y, int width, int height, Color color = black)
     {
         super(x, y, width, height, color);
     }
+    
     
 }
