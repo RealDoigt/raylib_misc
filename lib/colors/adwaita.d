830796434125cca3f47b698b3a2e99d4baebbe55
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-16 11:24:58 -0400

    adwaita accent colours

---
+++ lib/colors/adwaita.d
@@ -0,0 +1,10 @@
+module raylib_misc.colors.web;
+import raylib;
+
+// Accent Colours
+enum Color accentLight   = { 28, 113, 216};
+enum Color accentDark    = {120, 174, 237};
+enum Color accentBGLight = { 53, 132, 228};
+enum Color accentBGDark  = accentLight;
+enum Color accentFGLight = light1;
+enum Color accentFGDark  = light1;
