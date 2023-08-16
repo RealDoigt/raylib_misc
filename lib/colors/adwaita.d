7a1756b8deea42a39dfdb032c6df40c683acccf7
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-16 18:00:04 -0400

    refactor: reuse extant colours

--- lib/colors/adwaita.d
+++ lib/colors/adwaita.d
@@ -92,7 +92,7 @@ alias sidebarBGDark              = headerbarFGDark;
 alias sidebarFGLight             = windowFGLight;
 alias sidebarFGDark              = light1;
 enum Color sidebarBackdropLight  = {242, 242, 242, 255};
-enum Color sidebarBackdropDark   = { 42,  42,  42};
+enum Color sidebarBackdropDark   = { 42,  42,  42, 255};
 alias sidebarShadeLight          = shadeLight;
 alias sidebarShadeDark           = shadeDark;
 enum Color sidebar2BGLight       = {243, 243, 243, 255};
@@ -143,16 +143,16 @@ alias accentFGLight              = light1;
 alias accentFGDark               = light1;
 
 // Destructive Colours
-enum Color destructiveLight      = {192,  28,  40, 255};
+alias destructiveLight           = red4;
 enum Color destructiveDark       = {255, 123,  99, 255};
-enum Color destructiveBGLight    = {224,  27,  36, 255};
+alias destructiveBGLight         = red3;
 alias destructiveBGDark          = destructiveLight;
 alias destructiveFGLight         = light1;
 alias destructiveFGDark          = light1;
 
 // Success Colours
 enum Color successLight          = { 27, 133,  83, 255};
-enum Color successDark           = {143, 240, 164, 255};
+alias successDark                = green1;
 enum Color successBGLight        = { 46, 194, 126, 255};
 enum Color successBGDark         = { 38, 162, 105, 255};
 alias successFGLight             = light1;
@@ -161,7 +161,7 @@ alias successFGDark              = light1;
 // Warning Colours
 enum Color warningLight          = {156, 110,   3, 255};
 enum Color warningDark           = {248, 228,  92, 255};
-enum Color warningBGLight        = {229, 165,  10, 255};
+alias warningBGLight             = yellow5;
 enum Color warningBGDark         = {205, 147,   9, 255};
 alias warningFGLight             = windowFGLight;
 alias warningFGDark              = warningFGLight;
