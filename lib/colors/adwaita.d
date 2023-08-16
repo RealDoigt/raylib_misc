194bc59b5d888d738fe7323a047baa80c1d16680
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-16 17:33:17 -0400

    green

--- lib/colors/adwaita.d
+++ lib/colors/adwaita.d
@@ -2,16 +2,16 @@ module raylib_misc.colors.adwaita;
 import raylib;
 
 // Palette Colors
-enum Color blue1 = {153, 193, 241, 255};
-enum Color blue2 = { 98, 160, 234, 255};
-enum Color blue3 = { 53, 132, 228, 255};
-enum Color blue4 = { 28, 113, 216, 255};
-enum Color blue5 = { 26,  95, 180, 255};
-enum Color 
-enum Color 
-enum Color 
-enum Color 
-enum Color 
+enum Color blue1  = {153, 193, 241, 255};
+enum Color blue2  = { 98, 160, 234, 255};
+enum Color blue3  = { 53, 132, 228, 255};
+enum Color blue4  = { 28, 113, 216, 255};
+enum Color blue5  = { 26,  95, 180, 255};
+enum Color green1 = {143, 240, 164, 255};
+enum Color green2 = { 87, 227, 137, 255};
+enum Color green3 = { 51, 209, 122, 255};
+enum Color green4 = { 46, 194, 126, 255};
+enum Color green5 = { 38, 162, 105, 255};
 enum Color 
 enum Color 
 enum Color 
