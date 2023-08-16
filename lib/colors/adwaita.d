1e57c9d043bd70bdbf8c45c78c684fe3d21ed299
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-16 11:40:00 -0400

    success colours

--- lib/colors/adwaita.d
+++ lib/colors/adwaita.d
@@ -2,17 +2,25 @@ module raylib_misc.colors.adwaita;
 import raylib;
 
 // Accent Colours
-enum Color accentLight   = { 28, 113, 216};
-enum Color accentDark    = {120, 174, 237};
-enum Color accentBGLight = { 53, 132, 228};
+enum Color accentLight   = { 28, 113, 216, 255};
+enum Color accentDark    = {120, 174, 237, 255};
+enum Color accentBGLight = { 53, 132, 228, 255};
 enum Color accentBGDark  = accentBGLight;
 enum Color accentFGLight = light1;
 enum Color accentFGDark  = light1;
 
 // Destructive Colours
-enum Color destructiveLight   = {192,  28,  40};
-enum Color destructiveDark    = {255, 123,  99};
-enum Color destructiveBGLight = {224,  27,  36};
+enum Color destructiveLight   = {192,  28,  40, 255};
+enum Color destructiveDark    = {255, 123,  99, 255};
+enum Color destructiveBGLight = {224,  27,  36, 255};
 enum Color destructiveBGDark  = destructiveLight;
 enum Color destructiveFGLight = light1;
 enum Color destructiveFGDark  = light1;
+
+// Success Colours
+enum Color successLight   = { 27, 133,  83, 255};
+enum Color successDark    = {143, 240, 164, 255};
+enum Color successBGLight = { 46, 194, 126, 255};
+enum Color successBGDark  = { 38, 162, 105, 255};
+enum Color successFGLight = light1;
+enum Color successFGDark  = light1;
