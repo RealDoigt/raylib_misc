5e9a644ee92806a82ba2975c6c3d2ed14db00e54
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-16 11:31:23 -0400

    destructive colours

--- lib/colors/adwaita.d
+++ lib/colors/adwaita.d
@@ -1,10 +1,18 @@
-module raylib_misc.colors.web;
+module raylib_misc.colors.adwaita;
 import raylib;
 
 // Accent Colours
 enum Color accentLight   = { 28, 113, 216};
 enum Color accentDark    = {120, 174, 237};
 enum Color accentBGLight = { 53, 132, 228};
-enum Color accentBGDark  = accentLight;
+enum Color accentBGDark  = accentBGLight;
 enum Color accentFGLight = light1;
 enum Color accentFGDark  = light1;
+
+// Destructive Colours
+enum Color destructiveLight   = {192,  28,  40};
+enum Color destructiveDark    = {255, 123,  99};
+enum Color destructiveBGLight = {224,  27,  36};
+enum Color destructiveBGDark  = destructiveLight;
+enum Color destructiveFGLight = light1;
+enum Color destructiveFGDark  = light1;
