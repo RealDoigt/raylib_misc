2f442c34b135409ec8a9cf3ead058baf755fd204
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-17 07:12:59 -0400

    qt colours

---
+++ lib/colors/other.d
@@ -0,0 +1,23 @@
+module raylib_misc.colors.other;
+import raylib_misc.colors.web;
+import raylib;
+
+// Qt Colors
+alias qtWhite       = white;
+alias qtBlack       = black;
+alias qtCyan        = aqua;
+alias qtDarkCyan    = teal;
+alias qtRed         = red;
+alias qtDarkRed     = maroon;
+alias qtMagenta     = fuchsia;
+alias qtDarkMagenta = purple;
+alias qtGreen       = lime;
+alias qtDarkGreen   = green;
+alias qtYellow      = yellow;
+alias qtDarkYellow  = olive;
+alias qtBlue        = blue;
+alias qtDarkBlue    = navy;
+enum Color qtGray   = {160, 160, 164, 255};
+alias qtDarkGray    = gray;
+alias qtLightGray   = silver;
+alias qtTransparent = transparent;
